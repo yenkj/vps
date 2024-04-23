d260728d8f12b4be08df09f71c25099e50abfdbf
yenkj <49618620+yenkj@users.noreply.github.com>
2024-04-23 11:45:21 +0800

    Update docker_crack.sh

--- docker_crack.sh
+++ docker_crack.sh
@@ -2,15 +2,15 @@
 echo "docker一键**脚本，适4.8.5.0版本"
 echo "-------------------web端**-------------------"
 rm -f /system/Emby.Web.dll
-wget -P /system/ http://w.199301.xyz:9867/share/emby/linux_32_64/Emby.Web.dll
+wget -P /system/ https://gd.199301.xyz/share/emby/linux_32_64/Emby.Web.dll
 rm -f /system/MediaBrowser.Model.dll
-wget -P /system/ http://w.199301.xyz:9867/share/emby/linux_32_64/MediaBrowser.Model.dll
+wget -P /system/ https://gd.199301.xyz/share/emby/linux_32_64/MediaBrowser.Model.dll
 echo "核心**完成..."
 rm -f /system/dashboard-ui/modules/emby-apiclient/connectionmanager.js
-wget -P /system/dashboard-ui/modules/emby-apiclient/ http://w.199301.xyz:9867/share/emby/linux_32_64/dashboard-ui/modules/emby-apiclient/connectionmanager.js
+wget -P /system/dashboard-ui/modules/emby-apiclient/ https://gd.199301.xyz/share/emby/linux_32_64/dashboard-ui/modules/emby-apiclient/connectionmanager.js
 rm -f /system/dashboard-ui/embypremiere/embypremiere.js
-wget -P /system/dashboard-ui/embypremiere/ http://w.199301.xyz:9867/share/emby/linux_32_64/dashboard-ui/embypremiere/embypremiere.js
+wget -P /system/dashboard-ui/embypremiere/ https://gd.199301.xyz/share/emby/linux_32_64/dashboard-ui/embypremiere/embypremiere.js
 echo "web**完成..."
 rm -f /system/Emby.Server.Implementations.dll
-wget -P /system/ http://w.199301.xyz:9867/share/emby/linux_32_64/Emby.Server.Implementations.dll
+wget -P /system/ https://gd.199301.xyz/share/emby/linux_32_64/Emby.Server.Implementations.dll
 echo "Implementations替换认证..."
